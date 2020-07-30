651a8f4b94d2e5cc4d6467a635772dbbe92ccf8a
Aaron Morris <aaorris@live.com>
2020-07-29 21:42:29 -0700

    Remove logo

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,18 @@ import 'core-js/modules/es.promise.all-settled'
 import { css } from '@emotion/css'
 import styled from '@emotion/styled'
 import tw from '@tailwindcssinjs/macro'
-import AbortController from 'abort-controller'
 
-const Post = styled.div(tw`flex mb-6`)
-
-const Index = ({ items }) => (
+const Index = () => (
   <main className={css(tw`bg-white text-black`)}> 
-    <section className={css(tw`absolute flex justify-center top-6 items-center w-screen h-screen pointer-events-none`)}>
-      <div>
-      <svg class="vector" width="300px" height="300px" xmlns="http://www.w3.org/2000/svg" baseProfile="full" version="1.1" style={{fill:'none',stroke:'black',strokeWidth:5,strokeLinecap:'square'}}>
-        <g transform="translate(0,30)">
-          <g transform="translate(150,150),rotate(120,0,0)">
-            <path d="M0,-60 a60,60 0 1,0 0,120 l100,0"></path>   
-          </g>
-          <g transform="translate(150,150),rotate(240,0,0)">
-            <path d="M0,-60 a60,60 0 1,0 0,120 l100,0"></path>   
-          </g>
-          <g transform="translate(150,150),rotate(0,0,0)">
-            <path d="M0,-60 a60,60 0 1,0 0,120 l100,0"></path>   
-          </g>
-        </g>
-      </svg>
+    <section className={css(tw`w-screen h-screen pointer-events-none`)} style={{width: '100vw'}}>
+      <div className={css(tw`pt-6 text-center sm:text-6xl`)} style={{
+        fontWeight: 900, letterSpacing: '.2545ch'
+      }}>
+        YOU ARE
+        <br />
+        IN THE
+        <br />
+        HALLWAY
       </div>
     </section>
   </main>
